09f8d8a5d1c380e19ea68dec820a7bce1bc39f78
Hrmnnk <139231099+Hrmnnk@users.noreply.github.com>
2023-07-18 16:31:23 -0400

    casi terminado falta paso final

--- script.js
+++ script.js
@@ -1,39 +1,57 @@
-let intentos = 1;
-let palabra = "APPLE";
+let lista = ["ARBOL", "MESA","APLAUDIR","MUJER"];
+
+
+let intentos =  2;
+let palabra = lista[Math.floor(Math.random()*lista.length)];
+alert(palabra);
 const input = document.getElementById("guess-input");
 const button = document.getElementById("guess-button");
 const valor = input.value;
 button.addEventListener("click", intentar);
-function intentar() {
-  const INTENTO = leerIntento();
-  console.log(INTENTO);
-}
 function leerIntento() {
   let intento = document.getElementById("guess-input");
   intento = intento.value;
   intento = intento.toUpperCase();
   return intento;
 }
-for (let i in palabra) {
-  console.log(palabra[i]);
-}
+// for (let i in palabra) {
+//   console.log(palabra[i]);
+// }
 function intentar() {
   const INTENTO = leerIntento();
+  const GRID = document.getElementById("grid")
+  const ROW= document.createElement('div')
+  ROW.className="row"
   if (INTENTO === palabra) {
-    alert("GANASTE!");
+    
+    terminar("GANASTE!");
   }
   for (let i in palabra) {
-    if (INTENTO[i] === palabra[i]) alert("verde");
-    else if(palabra.includes(intentos[i])){
-        alert(intentos[i]+ "amarillo")
-    }
-      else{
-        alert(intentos[i]+ "gris" )
+    const SPAN= document.createElement('span')
+    SPAN.className= 'letter';
+    if (INTENTO[i] === palabra[i]){ 
+      SPAN.innerHTML= INTENTO[i]
+      SPAN.style.backgroundColor="green";
+    }else if(palabra.includes(INTENTO[i])){
+      SPAN.innerHTML= INTENTO[i]
+        SPAN.style.backgroundColor="yellow";
+    }else {
+      SPAN.innerHTML= INTENTO[i]
+        SPAN.style.backgroundColor="gray";
       }
-      intentos--
-      if (intentos==0){
-        terminae("FALLASTE >:D")
-      } 
-    
+      ROW.appendChild(SPAN)
   }
+  intentos--
+      if (intentos==0){
+        terminar("FALLASTE >:D")
+      } 
+
+      function terminar(mensaje){
+        button.disabled= true;
+        let contenedor = document.getElementById('guesses');
+        contenedor.innerHTML = mensaje;
+      }
+      GRID.appendChild(ROW)
 }
+
+
